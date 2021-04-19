bc894ff28136f291ce1f3129657c21158ee1563e
fathima murshida k <fathimakmurshida@gmail.com>
2021-04-19 15:18:44 +0530

    📦 NEW:

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class CoursesController {
-  @Get('/api/hello-worldd')
+  @Get('/api/hello-world')
   async helloWorld(): Promise<String> {
     return 'Hello Worlldd';
   }
